1f489196e29f85d2a9aa2726c5d740ee9ba7fa60
jshleap <jshleap@squalus.org>
2018-09-24 10:48:51 -0400

    Fix column names

--- join_processed_blasts.py
+++ join_processed_blasts.py
@@ -1,12 +1,12 @@
 import pandas as pd
 from glob import glob
-import sys
+import sys, os
 
 prefix=sys.argv[1]
 files=glob('%s*/*_number_of_reads_in_species.tsv' % prefix)
 df = []
 for f in files:
-    name = f.split('.')[0]
+    name = os.path.split(f)[0].split('.')[0]
     df.append(pd.read_table(f, sep='\t', index_col=0, header=None, names=[name]))
-df = pd.concat(df,axis=1)
+df = pd.concat(df,axis=1, sort=False)
 df.to_csv(sys.argv[2], sep='\t')
