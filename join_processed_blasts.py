b1754be6192d4cf1858f8464d743f59dbae04f09
jshleap <jshleap@squalus.org>
2018-09-24 11:01:42 -0400

    Add option for multiple files within folders

--- join_processed_blasts.py
+++ join_processed_blasts.py
@@ -23,7 +23,7 @@ import sys, os
 
 prefix=sys.argv[1]
 suffix=sys.argv[2]
-files=glob(os.join('*%s*' % prefix, '*.%s_number_of_reads_in_species.tsv' % suffix))
+files=glob(os.path.join('*%s*' % prefix, '*.%s_number_of_reads_in_species.tsv' % suffix))
 df = []
 for f in files:
     name = os.path.split(f)[0].split('.')[0]
