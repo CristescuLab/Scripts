06767caaf8372e86a307f044861d9cc6d69f8b10
jshleap <jshleap@squalus.org>
2018-09-24 10:59:56 -0400

    Add option for multiple files within folders

--- join_processed_blasts.py
+++ join_processed_blasts.py
@@ -1,12 +1,32 @@
+"""
+This script will take a set of folders with the results of a blast
+and a blast_processing.py results and concatenate them into a single
+ table with samples in the column.
+
+ Usage:
+
+ python join_processed_blasts.py <folder_pattern> <outputfile_suffix> <final_ouput_name>
+
+for example if you have a bunch of folders that start with output and within each one
+you have two parsing of blasts named test.98_98_number_of_reads_in_species.tsv, AND
+test.90_95_number_of_reads_in_species.tsv and if you do:
+
+python join_processed_blasts.py output 98_98 final.tsv
+
+This will create a final.tsv folder, concatenating all 98_98 files within the folder that
+contain output in their name
+
+"""
 import pandas as pd
 from glob import glob
 import sys, os
 
 prefix=sys.argv[1]
-files=glob('%s*/*_number_of_reads_in_species.tsv' % prefix)
+suffix=sys.argv[2]
+files=glob(os.join('*%s*' % prefix, '*.%s_number_of_reads_in_species.tsv' % suffix))
 df = []
 for f in files:
     name = os.path.split(f)[0].split('.')[0]
     df.append(pd.read_table(f, sep='\t', index_col=0, header=None, names=[name]))
 df = pd.concat(df,axis=1, sort=False)
-df.to_csv(sys.argv[2], sep='\t')
+df.to_csv(sys.argv[3], sep='\t')
