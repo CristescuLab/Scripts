fea4bec9caf9a73a1b230bbd259189be2c4e9cef
jshleap <jshleap@squalus.org>
2018-09-25 12:27:02 -0400

    Adding output name and grabbing the sample name properly

--- launch_submit.sh
+++ launch_submit.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 # This script is a simple for loop to launch multiple submit_blast_n_process.sh
 # jobs. It requires that the fasta to be blast be in a subfolder within where you will
-# be launching the job. It assumes that your folder is names into at least 5 field,
-# separated by ., where the 5th field if the name of your sample. The submit_blast_n_process.sh
+# be launching the job. It assumes that your folder is named with the last dot separated
+# field being of your sample. The submit_blast_n_process.sh
 # should be in your folder or in your path
 # Usage:
 # bash launch_submit.sh <pattern_folder> <database_with_path> <path_to_code>
 
 for i in `find . -type d -name "*$1*"`; do
-name=`echo ${i} | cut -d'.' -f5`
-sbatch --job-name=${name} --export=file_path=${i},db_path=$2 $3/submit_blast_n_process.sh
+name=`echo ${i} | rev | cut -d'.' -f 1 | rev`
+sbatch --job-name=${name} -e ${name}.out --export=file_path=${i},db_path=$2 $3/submit_blast_n_process.sh
 done
