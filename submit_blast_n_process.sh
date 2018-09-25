d5cb6c1a3a526adbf40f44d51dcb5896b631218f
jshleap <jshleap@squalus.org>
2018-09-25 12:51:45 -0400

    fixing outpu names

--- submit_blast_n_process.sh
+++ submit_blast_n_process.sh
@@ -19,7 +19,7 @@ cd $SLURM_SUBMIT_DIR
 
 #have a nice day! :)
 # The job command(s):
-out=`cut -d'.' -f5 <(echo ${file_path})`
+out=`echo ${i} | rev | cut -d'.' -f 1 | rev`
 path="${file_path}/${out}"
 
 do_blast=TRUE
