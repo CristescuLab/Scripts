5c8c972decdd7728f81a959d72cce26fd93c493f
jshleap <jshleap@squalus.org>
2018-09-25 10:00:43 -0400

    append intead of rewrite the #END

--- submit_blast_n_process.sh
+++ submit_blast_n_process.sh
@@ -37,7 +37,7 @@ if [[ ${do_blast} == TRUE ]]; then
     -evalue 0.0001 -perc_identity 97 -max_target_seqs 40 \
     -outfmt "6 qseqid sseqid pident evalue qcovs qlen length staxid stitle" \
     -out ${path}.hits -num_threads 8
-    echo -e "\n#END" > ${path}.hits
+    echo -e "\n#END" >> ${path}.hits
 fi
 python blast.processing.py ${path}.hits ${path}_98_98 -p 98 -q 98
 python blast.processing.py ${path}.hits ${path}_98_90 -p 98 -q 90
