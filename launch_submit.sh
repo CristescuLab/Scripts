6a048986811b0954381054f5cf8f7587dacb0f6b
jshleap <jshleap@squalus.org>
2018-09-28 13:25:12 -0400

    Making the looping more general and adding threads to the submit sccript

--- launch_submit.sh
+++ launch_submit.sh
@@ -1,13 +1,27 @@
 #!/usr/bin/env bash
-# This script is a simple for loop to launch multiple submit_blast_n_process.sh
+# This script is a simple for loop to launch multiple scripts
 # jobs. It requires that the fasta to be blast be in a subfolder within where you will
 # be launching the job. It assumes that your folder is named with the last dot separated
 # field being of your sample. The submit_blast_n_process.sh
 # should be in your folder or in your path
 # Usage:
-# bash launch_submit.sh <pattern_folder> <database_with_path> <path_to_code>
+# bash launch_submit.sh <pattern_folder> <database_with_path> <path_to_code> <cpus> <mem> <account>
+# <cpus> <mem> <account> are optional, defaulting to 8, 32G and def-mcristes
+pattern_folder=$1
+db=$2
+path_to_code=$3
+cpus=$4
+mem=$5
+acc=$6
 
-for i in `find . -type d -name "*$1*"`; do
+
+if [ -z ${cpus} ]; then cpus=8; fi
+if [ -z ${mem} ]; then mem=32G; fi
+if [ -z ${acc} ]; then acc=def-mcristes; fi
+
+for i in `find . -type d -name "*${pattern_folder}*"`; do
 name=`echo ${i} | rev | cut -d'.' -f 1 | rev`
-sbatch --job-name=${name} -o ${name}.out --export=file_path=${i},db_path=$2 $3/submit_blast_n_process.sh
+sbatch --job-name=${name} -o ${name}.out --cpus-per-task=${cpus}\
+--export=file_path=${i},db_path=${db},cpus=${cpus} --mem=${mem} \
+-A ${acc} ${path_to_code}
 done
--- submit_blast_n_process.sh
+++ submit_blast_n_process.sh
@@ -36,7 +36,7 @@ if [[ ${do_blast} == TRUE ]]; then
     blastn -db ${db_path} -query ${file_path}/*.trimmed.derep.fasta \
     -evalue 0.0001 -perc_identity 97 -max_target_seqs 40 \
     -outfmt "6 qseqid sseqid pident evalue qcovs qlen length staxid stitle" \
-    -out ${path}.hits -num_threads 8
+    -out ${path}.hits -num_threads ${cpus}
     echo -e "\n#END" >> ${path}.hits
 fi
 python blast_processing.py ${path}.hits ${path}_98_98 -p 98 -q 98
