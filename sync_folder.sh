1f812b691e406e68e76c1e7e89fcb303e268b211
jshleap <jshleap@squalus.org>
2018-09-28 12:51:39 -0400

    fixing source issue

--- sync_folder.sh
+++ sync_folder.sh
@@ -3,9 +3,8 @@
 ## USAGE: bash sync_software.sh <path2folder> <source_server> <target_server>
 # You need to be IN the folder you want to sync
 path2folder=$1
-source_server=$2
-target_server=$3
+target_server=$2
 
-echo "Syncing ${source_server}:${path2folder} to ${target_server}:${path2folder}"
+echo "Syncing ${path2folder} to ${target_server}:${path2folder}"
 rsync -rltv ${path2folder}/* ${target_server}:${path2folder}/
 rsync -rltv ${target_server}:${path2folder}/* ${path2folder}/
