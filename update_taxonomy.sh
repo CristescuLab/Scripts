ac12ecf78a17980965873d010552646a906e73f1
jshleap <jshleap@squalus.org>
2018-10-05 12:24:13 -0400

    changing the grep and the way the presence/absense is handled

--- update_taxonomy.sh
+++ update_taxonomy.sh
@@ -2,16 +2,21 @@
 
 tmp_folder=$1
 cpus=4
+tmp=${tmp_folder}/tmp
 echo bye | lftp -e "mirror -c" ftp://ftp.ncbi.nlm.nih.gov/pub/taxonomy/accession2taxid/
 echo "will cite" | parallel --citation
 for i in *.gz; do
-    a=`zcat ${i} | cut -f1-3 | tee tmp| tail -n 1| cut -f 1`
-    if LC_ALL=C fgrep -q -m 1 "${a}" accession2taxid; then
+    echo "Processing ${i}"
+    #a=`zcat ${i} | tail -n +2| cut -f1-3 | tee ${tmp}| tail -n 1| cut -f1,2`
+    a=`zcat ${i} | tail -1| cut -f1,2`
+    if LC_ALL=C grep -q -m 1 "^${a}" accession2taxid.lineage; then
     echo "$i has previously been done"; else
-    parallel -j ${cpus} --tmpdir ${tmp_folder} --pipepart -a tmp --block 500m \
-    'taxonkit lineage -i 3 | taxonkit reformat -i 4' >> accession2taxid
-    rm tmp
+    #zcat ${i} | tail -n +2| cut -f1-3 > ${tmp}
+    parallel -j ${cpus} --tmpdir ${tmp_folder} --pipepart \
+    -a <(zcat ${i} | tail -n +2| cut -f1-3) --block 500m \
+    'taxonkit lineage -i 3 | taxonkit reformat -i 4' >> accession2taxid.lineage
+    rm ${tmp}
     fi
 done
-sort -T ${tmp_folder} -u accession2taxid > accession2taxid.lineage && \
-rm accession2taxid tmp
+sort -T ${tmp_folder} -u accession2taxid.lineage > tmp && \
+mv tmp accession2taxid.lineage && rm ${tmp} tmp
