2b4bc967a9c3048d10de6f698077f2c1a74e88a9
jshleap <jshleap@squalus.org>
2018-10-09 16:04:54 -0400

    adding get sequences

--- get_sequences.sh
+++ get_sequences.sh
@@ -3,8 +3,9 @@
 # Usage bash get_sequences.sh <column of taxlevel> <name of filtered file>
 ## ALEXA
 prefix=${2%%_filtered.tsv}
-for i in `cut -f $1 $2 | sort -u| awk NF`;do
+cut -f $1 $2 | sort -u| awk NF > ${prefix}.species
+while read i;do
  ~/projects/def-mcristes/Software/seqkit grep -n -r -f <(grep "${i}" $2| \
  cut -f 1) ${prefix}.clean.fasta > ${prefix}_${i}.fasta
-done
+done<${prefix}.species
 
