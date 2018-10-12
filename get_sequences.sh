e29cfad63c06567e8e9e89a3adb8c0361708854e
jshleap <jshleap@squalus.org>
2018-10-12 16:45:08 -0400

    including muscle in get sequences, and new script for nucleotide diversity

--- get_sequences.sh
+++ get_sequences.sh
@@ -6,7 +6,7 @@ prefix=${2%%_filtered.tsv}
 cut -f $1 $2 | sort -u| awk NF > ${prefix}.species
 while read i;do
  outfn=`echo ${prefix}_${i}.fas|tr ' ' '_'`
- ~/projects/def-mcristes/Software/seqkit grep -n -r -f <(grep "${i}" $2| \
- cut -f 1) ${prefix}.clean.fasta | muscle> ${outfn}
+ seqkit grep -n -r -f <(grep "${i}" $2| cut -f 1) ${prefix}.clean.fasta | \
+ muscle> ${outfn}
 done<${prefix}.species
 
--- nucleotide_diversity.R
+++ nucleotide_diversity.R
@@ -3,17 +3,25 @@
 # TAKEN AND MODIFIED FROM https://github.com/sophiadavid1/nucleotide_diversity_analysis
 # Created by: jshleap
 # Created on: 10/12/18
+# In cedar need to load:
+# module load gcc/5.4.0 r-bundle-bioconductor/3.4
 
 require(pegas)
 require(ape)
 
-file.names <- dir(pattern=".fasta")
+file.names <- dir(pattern=".fas$")
 args = commandArgs(trailingOnly=TRUE)
 
+df <- data.frame(matrix(ncol = 2, nrow = 0))
+x <- c("File", "pi")
+colnames(df) <- x
+j=1
 for(i in 1:length(file.names)){
 	gene <- read.dna(file.names[i], format="fasta", as.matrix=FALSE)
+    if (length(gene) > 2){
 	pi <- nuc.div(gene)
-	write.table(file.names[i], file=args[1], append=TRUE)
-	write.table(pi, file=args[1], append=TRUE)
-
+	df[j,"File"] = file.names[i]
+    df[j,"pi"] = pi
+    j = j+1}
 }
+write.table(df, args[1], sep='\t')
