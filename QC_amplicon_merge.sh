93610cac77a158e6159ee8b8735a2846e52abd7a
jshleap <jshleap@squalus.org>
2018-10-29 15:51:18 -0400

    Including fastqc per step and outdir solution

--- QC_amplicon_merge.sh
+++ QC_amplicon_merge.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # usage: QC_amplicon_merge.sh primer_fwd primer_rev R1_filename outputdir
 # for now only tested with CO1
+# If used on computecanada:
+# module load trimmomatic fastqc intel/2016.4 bbmap/37.36 pear/0.9.10
 
 qc(){
 # create output folder if required
@@ -14,7 +16,7 @@ data=`basename ${3%%_R1.fastq.gz}`
 data_folder=`dirname $3`
 
 # Run fastqc on the raw reads
-fastqc ${data_folder}/${data}_R*.fastq.gz -o ${4}
+fastqc ${data_folder}/${data}_R[12].fastq.gz -o ${4}
 #runtrimmo
 adapters=${EBROOTTRIMMOMATIC}/adapters/NexteraPE-PE.fa
 java -jar ${EBROOTTRIMMOMATIC}/trimmomatic-0.36.jar PE -phred33 \
@@ -24,11 +26,11 @@ ${4}/${data}_R2_2P.fastq.gz ${4}/${data}_R2_2U.fastq.gz \
 ILLUMINACLIP:${adapters}:3:30:6 SLIDINGWINDOW:10:30
 
 # Run fastqc on the trimmomatic output
-fastqc ${4}/${data}_R*_*.fastq.gz -o ${4}
+fastqc ${4}/${data}_R[12]_[12][UP].fastq.gz -o ${4}
 
 # run filter tile
-filterbytile.sh in1=${4}/${data}_R1_1P.fastq.gz in2=${4}${data}_R2_2P.fastq.gz \
-out1=${4}/${data}.R1.fastq.gz out2=${4}/${data}.R2.fastq.gz
+filterbytile.sh in1=${4}/${data}_R1_1P.fastq.gz in2=${4}/${data}_R2_2P.fastq.gz \
+out1=${4}/${data}.R1.fastq.gz out2=${4}/${data}.R2.fastq.gz usejni=t
 
 # run fastqc on the filter tile
 fastqc ${4}/${data}.R*.fastq.gz -o ${4}
@@ -46,31 +48,32 @@ pear -f ${4}/${data}.1.fastq.gz -r ${4}/${data}.2.fastq.gz \
 -o ${4}/${data} -q 28 -t 150 -n 200 -m 600 -j 8 -e 2 -p 0.01 -v 20
 
 # run fastqc on the pear  output
-fastqc ${4}/${data}*assembled* -o ${4}
+fastqc ${4}/${data}*assembled*.fastq -o ${4}
 
 # Subset and deduplicate
-seqkit -j 10 subseq -r 1:360 ${data}.assembled.fastq | seqkit -j 10 rmdup \
--s -i -o ${4}/${data}.clean.fastq -d ${pref}_duplicated.fastq.gz \
--D ${pref}_duplicated.detail.txt
+seqkit -j 10 subseq -r 1:360 ${4}/${data}.assembled.fastq | seqkit -j 10 rmdup \
+-s -i -o ${4}/${data}.clean.fastq -d ${4}/${data}_duplicated.fastq.gz \
+-D ${4}/${data}_duplicated.detail.txt
+
 seqkit -j 10 fq2fa ${4}/${data}.clean.fastq -o ${4}/${data}.clean.fasta
 
 # deduplicate unasembled forward
 seqkit -j 10 rmdup -s -i -o ${4}/${data}.unassembled.forward.clean.fastq \
--d ${pref}.unassembled.forward.duplicated.fastq.gz \
--D ${pref}.unassembled.forward.duplicated.detail.txt \
-${data}.unassembled.forward.fastq
+-d ${data}.unassembled.forward.duplicated.fastq.gz \
+-D ${data}.unassembled.forward.duplicated.detail.txt \
+${4}/${data}.unassembled.forward.fastq
 
 seqkit -j 10 fq2fa ${4}/${data}.unassembled.forward.clean.fastq \
 -o ${4}/${data}.unassembled.forward.clean.fasta
 
 # deduplicate unasembled reverse
 seqkit -j 10 rmdup -s -i -o ${4}/${data}.unassembled.reverse.clean.fastq \
--d ${pref}.unassembled.reverse.duplicated.fastq.gz \
--D ${pref}.unassembled.reverse.duplicated.detail.txt \
+-d ${4}/${data}.unassembled.reverse.duplicated.fastq.gz \
+-D ${4}/${data}.unassembled.reverse.duplicated.detail.txt \
 ${data}.unassembled.forward.fastq
 
-seqkit -j 10 {4}/${data}.unassembled.reverse.clean.fastq \
--o {4}/${data}.unassembled.reverse.clean.fasta
+seqkit -j 10 fq2fa ${4}/${data}.unassembled.reverse.clean.fastq \
+-o ${4}/${data}.unassembled.reverse.clean.fasta
 
 # run fastqc on the last output
 fastqc ${4}/${data}.clean.fastq -o ${4}
