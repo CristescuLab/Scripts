8ef4f9b01954dc6a38582263d7cb2bbc16ce5b2e
jshleap <jshleap@squalus.org>
2019-08-02 12:57:24 -0400

    Fixing the sample delimiter ossie

--- ASV_pipeline.sh
+++ ASV_pipeline.sh
@@ -63,38 +63,41 @@ run_QC_single_pair(){
 # 5) Path to output directory
 # 6) prefix of output
 # 7) Prefix of the fileset
+# 8) cpus
 
 # run first cutadapt
-cutadapt -g "${1}" -G "${2}" -a "${3}" -A "${4}" -o ${5}/${6}.1.fastq.gz \
--p ${5}/${6}.2.fastq.gz -m 130 --match-read-wildcards -q 25 --trim-n \
--n 2 --untrimmed-output ${5}/untrimmed.${6}.fastq --untrimmed-paired-output \
-${5}/untrimmed.paired.${6}.fastq ${7}_R1.fastq.gz \
-${7}_R2.fastq.gz > ${5}/${6}.log1
+fileset=`basename ${7}`
+sample=`echo ${fileset}|rev|cut -d '.' -f1|rev`
+cutadapt -g "${1}" -G "${2}" -a "${3}" -A "${4}" \
+-o ${5}/${sample}.1.fastq.gz -p ${5}/${sample}.2.fastq.gz \
+-m 130 --match-read-wildcards -q 25 --trim-n -n 2 --untrimmed-output \
+${5}/untrimmed.${sample}.fastq --untrimmed-paired-output \
+${5}/untrimmed.paired.${sample}.fastq ${7}_R1.fastq.gz \
+${7}_R2.fastq.gz > ${5}/${sample}.log1
 # Merge reads
-pear -f ${5}/${6}.1.fastq.gz -r ${5}/${6}.2.fastq.gz -o ${5}/${6}_pear -q 25 \
--t 100 -s 2 > ${5}/${6}_pear.log
+pear -f ${5}/${sample}.1.fastq.gz -r ${5}/${sample}.2.fastq.gz \
+-o ${5}/${sample}_pear -q 25 -t 100 -s 2 > ${5}/${sample}_pear.log
 # check if adapters still there
-seqkit -j 8 locate -d -p "${1}" ${5}/${6}_pear.assembled.fastq > ${5}/${6}.fwd
-seqkit -j 8 locate -d -p "${2}" ${5}/${6}_pear.assembled.fastq > ${5}/${6}.rev
+seqkit -j ${8} locate -d -p "${1}" ${5}/${sample}_pear.assembled.fastq > ${5}/${sample}.fwd
+seqkit -j ${8} locate -d -p "${2}" ${5}/${sample}_pear.assembled.fastq > ${5}/${sample}.rev
 # cut the adapter if they remain
-if [[ $(wc -l <${5}/${6}.fwd) -ge 2 || $(wc -l <${5}/${6}.rev) -ge 2 ]]; then
-cutadapt -a "$3" -m 100 -n 2 --too-short-output ${5}/${6}.short.fastq \
--o ${5}/${6}.3trimmed.fastq ${5}/${6}_pear.assembled.fastq > ${5}/${6}.log2
-cutadapt -g "${1}" -n 2 -o ${5}/${6}.5trimmed.fastq \
-${5}/${6}.3trimmed.fastq > ${outdir}/${6}.log3; else
-cp ${5}/${6}_pear.assembled.fastq ${5}/${6}.3trimmed.fastq
+if [[ $(wc -l <${5}/${sample}.fwd) -ge 2 || $(wc -l <${5}/${sample}.rev) -ge 2 ]]; then
+cutadapt -a "$3" -m 100 -n 2 --too-short-output ${5}/${sample}.short.fastq \
+-o ${5}/${sample}.3trimmed.fastq ${5}/${sample}_pear.assembled.fastq > ${5}/${sample}.log2
+cutadapt -g "${1}" -n 2 -o ${5}/${sample}.5trimmed.fastq \
+${5}/${sample}.3trimmed.fastq > ${outdir}/${sample}.log3; else
+cp ${5}/${sample}_pear.assembled.fastq ${5}/${sample}.3trimmed.fastq
 fi
 # dereplicate
-usearch -fastx_uniques ${5}/${6}.3trimmed.fastq -fastaout \
-${5}/${6}.trimmed.derep.fasta -sizeout
+usearch -fastx_uniques ${5}/${sample}.3trimmed.fastq -fastaout \
+${5}/${sample}.trimmed.derep.fasta -sizeout
 # get lenghth distributions
-length_stats ${5}/${6}.trimmed.derep.fasta ${5}/${6}
+length_stats ${5}/${sample}.trimmed.derep.fasta ${5}/${6}
 # get stats for all the steps
-seqkit stats ${5}/*${6}*.fa* > ${5}/${6}.stats
+seqkit -j ${8} stats ${5}/*${sample}*.fa* > ${5}/${6}.stats
 # run a fastqc
-fastqc ${5}/${6}.3trimmed.fastq -o ${5}
+fastqc ${5}/${sample}.3trimmed.fastq -o ${5}
 }
-
 
 # Get the command line arguments
 file_list=$1
@@ -105,31 +108,33 @@ prefix=$5
 cpus=$6
 min_len=$7
 max_len=$8
-outdir=${prefix}_output
+outdir=${prefix}_output${primer_name}
 
-
+echo `date`
+echo "executing $0 $@"
 # Process the primers and get reverse complements
 Adapter1rc=`echo $ADAPTER_FWD | tr 'ACGTYRSWKMBDHV' 'TGCARYSWMKVHDB' | rev`
 Adapter2rc=`echo $ADAPTER_REV | tr 'ACGTYRSWKMBDHV' 'TGCARYSWMKVHDB' | rev`
 # create output folder
-mkdir -p ${outdir}${primer_name}
-outdir=${outdir}${primer_name}
+mkdir -p ${outdir}
 
 # run QC in all pairs
-export -f run_QCsingle_pair
-parallel --will-cite --joblog ${outdir}/${prefix}_QC.log -j ${cpus} --wd . \
-run_QCsingle_pair ${ADAPTER_FWD} ${ADAPTER_REV} ${Adapter2rc} ${Adapter1rc} \
-${outdir} ${prefix} {} :::: ${file_list}
+while read line
+do
+    run_QC_single_pair ${ADAPTER_FWD} ${ADAPTER_REV} ${Adapter2rc} \
+    ${Adapter1rc} ${outdir} ${prefix} ${line} ${cpus}
+done <${file_list}
+
 
 # Trim the reads to expected lenght
 for i in ${outdir}/*3trimmed.fastq
 do
     cat ${i} | seqkit -j ${cpus} seq -m ${min_len} \
-    -M ${max_len} > $outdir/{i%%3trimmed.fastq}lengthfilter.fastq
+    -M ${max_len} >${i%%3trimmed.fastq}lengthfilter.fastq
 done
 
 # Concatenate all resulting files
-cat ${outdir}*.lengthfilter.fastq >> ${outdir}/all.lengthfilter.fastq
+cat ${outdir}/*.lengthfilter.fastq > ${outdir}/all.lengthfilter.fastq
 
 # Dereplicate on full set reporting size of identical sequences
 vsearch --derep_fulllength  ${outdir}/all.lengthfilter.fastq -sizein -sizeout \
@@ -138,16 +143,15 @@ vsearch --derep_fulllength  ${outdir}/all.lengthfilter.fastq -sizein -sizeout \
 # Run the unoise3 denoising
 usearch -unoise3 ${outdir}/vs_all_lengthfilter.fasta -zotus \
 ${outdir}/all_lengthfilter_zotus_4.fasta -minsize 4 -tabbedout \
-${outdir}/allzotus_lengthfilter_4.txt -sizeout
+${outdir}/allzotus_lengthfilter_4.txt
 
 # Rename to Zotu
-sed -i "/>O/>Zo/" ${outdir}/all_lengthfilter_zotus_4.fasta
+sed -i "s/>O/>Zo/g" ${outdir}/all_lengthfilter_zotus_4.fasta
 
 # reannotate each sample
-parallel
 for i in ${outdir}/*lengthfilter.fastq
 do
-    usearch -fastx_relabel ${i} -prefix ${i%%.lengthfilter.fastq} \
+    usearch -fastx_relabel ${i} -prefix `basename ${i%%.lengthfilter.fastq}`_ \
     -fastqout ${i%%.fastq}_relabel.fastq -keep_annots
 done
 
@@ -155,10 +159,10 @@ done
 usearch -makeudb_usearch ${outdir}/all_lengthfilter_zotus_4.fasta -output \
 ${outdir}/all_lengthfilter_zotus_4.udb
 
-cat ${outdir}/*lengthfilter_relabel.fastq >> ${outdir}/all.lengthfilter_relabel.fastq
+cat ${outdir}/*lengthfilter_relabel.fastq > ${outdir}/all.lengthfilter_relabel.fastq
 
 # Created the Zotu table with the renamed samples
-usearch -otutab ${outdir}/all.lengthfilter_relabel.fastq -sample_delim _ \
+usearch -otutab ${outdir}/all.lengthfilter_relabel.fastq  -sample_delim _ \
 -zotus ${outdir}/all_lengthfilter_zotus_4.udb -otutabout \
 ${outdir}/all_${primer_name}zotutab_4.txt -mapout \
 ${outdir}/all_${primer_name}zmap_4.txt
