1f9229527e86cb582bf410c47ffb978ff6babcb6
jshleap <jshleap@squalus.org>
2019-12-08 16:06:50 -0500

    Adding script to install basta in computecanada

---
+++ install_basta_compute_canada.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+module purge
+module load nixpkgs/16.09  gcc/7.3.0 leveldb/1.20 scipy-stack/2018b python/2.7.14
+pip install Plyvel --user
+pip install git+https://github.com/timkahlke/BASTA.git --user
+basta taxonomy
+basta download gb
+echo "BEFORE YOU RUN BASTA, EXECUTE:"
+echo "module purge; module load nixpkgs/16.09  gcc/7.3.0 leveldb/1.20 scipy-stack/2018b python/2.7.14"
